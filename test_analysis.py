7b8616bf47ee92a129d969fae823fbd953eb2782
Christine Kaufhold <christinekaufhold24@gmail.com>
2020-12-14 13:00:00 +0100

    Update test_analysis.py

--- test_analysis.py
+++ test_analysis.py
@@ -1,10 +1,31 @@
 import numpy as np
-import matplotlib.pylab as pl
-import os
+import matplotlib.pylab as plt
 import xarray
+# import os
 
 # Programmed by Christine Kaufhold, IfM 2020
 
 # %% Set- Up
 
-x= xarray.open_dataset("pyOM.cdf")
+data = xarray.open_dataset("basin_3d_pyOM.cdf")
+temp = data["temp"] # Temperature(t,z,y,x)
+U = data["u"]
+
+# %% Tank dimensions
+x = np.arange(60)*0.01
+y = np.arange(60)*0.01
+z = np.arange(20)*0.01
+
+# %% Plot T(x,y) at z = 10cm
+    
+for t in range(5):
+    plt.figure()
+    print("Time Step " + str(t+1) + "/100")
+    plt.pcolormesh(x,y,U[t,10,:,:])
+    #plt.colorbar(label='T [°C]')
+    plt.title("temperature in x-y plane at z = 3cm")
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    #plt.savefig('img/' + file_name, format='png')
+    plt.show()
+    plt.close
